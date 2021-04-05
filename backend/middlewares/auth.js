f05dec8d3b90c24898f6f5a2b4ea427a534a2cf4
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-04-05 21:50:17 +0800

    Roles authorization and product routes protection

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,9 @@
-//  Check if user is authenticated
 import jwt from "jsonwebtoken";
 import User from "../models/user.model";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "./catchAsyncErrors";
 
+//  Check if user is authenticated
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   if (!token)
@@ -12,6 +12,21 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
     );
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = User.findById(decoded.id);
+  req.user = await User.findById(decoded.id);
   next();
 });
+
+// Handle user roles authorization
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user.role}) is not authorized to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -7,16 +7,18 @@ import {
   updateProduct,
 } from "../controllers/product.controller";
 
-import { isAuthenticated } from "../middlewares/auth";
+import { isAuthenticated, authorizeRoles } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.route("/products").get(getProducts);
+router.route("/products").get(isAuthenticated, getProducts);
 router.route("/products/:id").get(getProductById);
-router.route("/admin/products/new").post(isAuthenticated, newProduct);
+router
+  .route("/admin/products/new")
+  .post(isAuthenticated, authorizeRoles("admin"), newProduct);
 router
   .route("/admin/products/:id")
-  .put(isAuthenticated, updateProduct)
-  .delete(isAuthenticated, deleteProduct);
+  .put(isAuthenticated, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticated, authorizeRoles("admin"), deleteProduct);
 
 export default router;
