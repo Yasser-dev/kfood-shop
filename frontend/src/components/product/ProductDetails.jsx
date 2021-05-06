1ba829f8814b8e59c17f7b9d11fb2f7ae55c68c3
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-05-06 23:55:34 +0800

    product page increase/decrease quantity

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Carousel } from "react-bootstrap";
 import Loader from "../layouts/Loader";
@@ -11,6 +11,7 @@ import {
   dismissProduct,
 } from "../../actions/productActions";
 const ProductDetails = ({ match }) => {
+  const [quantity, setQuantity] = useState(1);
   const dispatch = useDispatch();
   const alert = useAlert();
 
@@ -28,6 +29,25 @@ const ProductDetails = ({ match }) => {
       dispatch(dismissProduct());
     };
   }, [dispatch, alert, error, match.params.id]);
+
+  const increaseQty = () => {
+    const count = document.querySelector(".count");
+    if (count.valueAsNumber >= product.stock) {
+      return;
+    }
+
+    const qty = count.valueAsNumber + 1;
+    setQuantity(qty);
+  };
+  const decreaseQty = () => {
+    const count = document.querySelector(".count");
+    if (count.valueAsNumber <= 1) {
+      return;
+    }
+
+    const qty = count.valueAsNumber - 1;
+    setQuantity(qty);
+  };
 
   return (
     <Fragment>
@@ -70,21 +90,26 @@ const ProductDetails = ({ match }) => {
 
               <p id="product_price">EGP {product.price} </p>
               <div className="stockCounter d-inline">
-                <span className="btn btn-danger minus">-</span>
+                <span className="btn btn-danger minus" onClick={decreaseQty}>
+                  -
+                </span>
 
                 <input
                   type="number"
                   className="form-control count d-inline"
-                  value="1"
+                  value={quantity}
                   readOnly
                 />
 
-                <span className="btn btn-primary plus">+</span>
+                <span className="btn btn-primary plus" onClick={increaseQty}>
+                  +
+                </span>
               </div>
               <button
                 type="button"
                 id="cart_btn"
                 className="btn btn-primary d-inline ml-4"
+                disabled={product.stock === 0}
               >
                 Add to Cart
               </button>
@@ -92,13 +117,16 @@ const ProductDetails = ({ match }) => {
               <hr />
 
               <p>
-                Status:{" "}
+                Status:
                 <span
                   id="stock_status"
                   className={product.stock > 0 ? "greenColor" : "redColor"}
                 >
                   {product.stock > 0 ? "In Stock" : "Out of Stock"}
                 </span>
+                {product.stock > 0 && (
+                  <small className="ml-2">({product.stock} Available)</small>
+                )}
               </p>
 
               <hr />
